5cb0bbad62a77ce119dcf2f2268407abf71433d1
elvis sylejmani <elvissylejmani13@gmail.com>
2019-08-05 14:56:25 +0200

    NewTabel&Fresh

---
+++ database/migrations/2019_08_05_125541_create_shtepia_botueses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShtepiaBotuesesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shtepia_botueses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shtepia_botueses');
+    }
+}
