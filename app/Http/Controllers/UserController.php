65c50a3ac674a6f824f2ce13281b02b94e65d042
elvis sylejmani <elvissylejmani13@gmail.com>
2019-08-12 14:29:46 +0200

    Search

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace LSM\Http\Controllers;
 use Illuminate\Http\Request;
 use LSM\User;
 use Illuminate\Support\Facades\Hash;
+use LSM\Http\Controllers\Input;
 class UserController extends Controller
 {
     /**
@@ -14,7 +15,15 @@ class UserController extends Controller
      */
     public function index()
     {
+        $q = User::query();
         $users = User::all();
+        $name = request('Search');
+        if($name != null)
+        {
+            $q->where('Emri','like', $name);
+            $users = $q->orderBy('id')->paginate(100);
+            return view('insert',compact('users'));
+        }
         return view('insert',compact('users'));
     }
     /**
