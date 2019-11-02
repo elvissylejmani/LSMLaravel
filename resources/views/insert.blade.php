959945251855f9c8ed2a1f6b035ec8d73240e832
elvis sylejmani <elvissylejmani13@gmail.com>
2019-11-02 21:01:45 +0100

    FrontEndUpdated

--- resources/views/insert.blade.php
+++ resources/views/insert.blade.php
@@ -1,3 +1,4 @@
+@extends('layouts.app')
 <?php
 use LSM\User;
 ?>
@@ -10,6 +11,8 @@ use LSM\User;
     <title>Document</title>
 </head>
 <body>
+@section('content')
+    <div class="container">
     <form action="Admin" method="POST">
         @csrf
         <input type="text" name="Emri" value="{{ old('Emri')}}" id="">
@@ -48,32 +51,48 @@ use LSM\User;
 
         <a href="/Roli"> <h4>Shto Role:</h4> </a>
     @if ($users->Count())
-    <div class="users" style="float:left">
-Userat:
-<ul>
+    <!--<div class="users" style="float:left"> -->
+        <div class="container">
+        <div class="row">
+<div class="col-sm">
+        <p class="h1"> Userat:</p>
+        </div>
+        </div>
+        </div>
+        <div class="container">
+     <div class="row align-items-start">
     @foreach ($users as $user)
-     <li>   {{$user->Emri}} </li>
-      <li>   {{$user->Mbiemri}}  </li>
-      <li>   {{$user->NrPersonal}}</li>
-      <li>   {{$user->NrIdentifikues}}</li>
-     <li>    {{$user->DataELindjes}}</li>
-     <li> @foreach (User::findOrFail($user->id)->Roli as $roli)
+   
+        <div class="col-md-4 mb-3">
+        <nav class="navbar navbar-md navbar-light bg-white shadow-sm align-start border border-primary rounded">
+     <ul class="navbar-nav auto">
+
+    <li class="nav-item">   {{$user->Emri}} </li> 
+    <li class="nav-item">   {{$user->Mbiemri}}  </li>
+    <li class="nav-item">   {{$user->NrPersonal}}</li>
+    <li class="nav-item">   {{$user->NrIdentifikues}}</li>
+    <li class="nav-item">    {{$user->DataELindjes}}</li>
+    <li class="nav-item"> @foreach (User::findOrFail($user->id)->Roli as $roli)
         {{$roli->Roli}}
     @endforeach</li>
-     @if ($user->Librat != null)<li>{{$user->Librat}}</li>@endif
-     <li>   {{$user->email}}</li>
-     <li><a href="Admin/{{$user->id}}">Edit</a></li>
-     <li> <form action="Admin/{{$user->id}}" method="POST">
+     @if ($user->Librat != null)<li class="nav-item" >{{$user->Librat}}</li>@endif
+     <li class="nav-item">   {{$user->email}}</li>
+     <li class="nav-item w-100"><a href="Admin/{{$user->id}}" class="float-sm-left"><button type="button" class="btn btn-primary float-left">Edit</button></a>
+     <form action="Admin/{{$user->id}}" method="POST" class="float-right">
        @csrf
        @method('DELETE') 
-       <button type="submit">Delete</button>
+       <button type="submit" class="btn btn-danger float-right" >Delete</button>
     </form></li>
-   @endforeach
-     
-     
-     
 </ul>
-@endif
+        </nav>
+        </div>
+   @endforeach
+   </div>
 </div>
+@endif
+<!--</div> -->
+</div>
+@endsection
+
 </body>
 </html>
