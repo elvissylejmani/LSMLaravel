84aa800e7449ae538f9b13f9bec34f20f5418e3f
elvis sylejmani <elvissylejmani13@gmail.com>
2019-11-02 18:23:29 +0100

    HashingPasswordWithBcrypt

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,9 +44,7 @@
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                            
                             @endif
                         @else
                             <li class="nav-item dropdown">
@@ -60,7 +58,7 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
+                                    
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
