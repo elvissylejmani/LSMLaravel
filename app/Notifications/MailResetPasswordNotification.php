6a9b02504737daa7cb8b86ea27945c078e077b5a
elvis sylejmani <elvissylejmani13@gmail.com>
2019-11-04 17:28:44 +0100

    ResetPasswordEmailCustomized

---
+++ app/Notifications/MailResetPasswordNotification.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace LSM\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class MailResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    // public function toMail($notifiable)
+    // {
+    //     return (new MailMessage)
+    //                 ->line('The introduction to the notification.')
+    //                 ->action('Notification Action', url('/'))
+    //                 ->line('Thank you for using our application!');
+    // }
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject("Reset your password")
+                    ->line("Pershendetje, Keni harruar fjalëkalimin? kliko në butonin më posht për ta rikthyer atë!")
+                    ->action('Reset Password', url('password/reset', $this->token))
+                    ->line('Nqofse nuk keni kerkuar ta riktheni fjalëkalimin mos nderrmerni hapa të më tejshëm!')
+                    ->line('Faliminderit për mirkuptim.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
