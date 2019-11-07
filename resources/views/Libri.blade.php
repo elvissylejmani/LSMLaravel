74aac7116dadcf9f9c3d23b97d12b55269a4e697
elvis sylejmani <elvissylejmani13@gmail.com>
2019-11-07 13:34:35 +0100

    FrontEndEdited

--- resources/views/Libri.blade.php
+++ resources/views/Libri.blade.php
@@ -9,54 +9,80 @@ use LSM\Libri;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+    ul {
+        list-style-type: none;
+    }
+    </style>
 </head>
 <body>
     @section('content')
+    <div class="container-fluid">
+
     <form action="librat" method="POST">
         @csrf
-        <table>
-        <th>Numri Serik</th>
-        <td><input type="text" name="NumriSerik" value="{{ old('NumriSerik')}}" id=""></td>
-        <th>Foto e Librit</th>
-        <td> <input type="text" name="FotoLibrit" value="{{ old('FotoLibrit')}}" id=""></td>
-        <th>Zhanri</th>
-        <td>  <input type="text" name="ID_Zhanri" value="{{ old('ID_Zhanri')}}" id=""></td>
-        <th>Titulli</th>
-        <td> <input type="text" name="Titulli" value="{{ old('Titulli')}}" id=""></td>
-        <th>Autori</th>
-        <td> <input type="text" name="Autori" value="{{ old('Autori')}}" id=""></td>
-        <th>Shtepia Botuese</th>
-        <td> <input type="text" name="ID_ShtepiaBotuese" value="{{ old('ID_ShtepiaBotuese')}}" id=""></td>
+        <ul>
+            <div class="row">
+                    <nav class="navbar navbar-md navbar-light bg-white shadow-sm align-start border border rounded">
+            <div class="col">
+        <li class="nav-item">Numri Serik</li>
+        <li><input type="text" name="NumriSerik" value="{{ old('NumriSerik')}}" id=""></li>
+        <li>Foto e Librit</li>
+        <li> <input type="text" name="FotoLibrit" value="{{ old('FotoLibrit')}}" id=""></li>
+        <li>Zhanri</li>
+        <li>  <input type="text" name="ID_Zhanri" value="{{ old('ID_Zhanri')}}" id=""></li>
+    </div>
+            <div class="col">
+        <li>Titulli</li>
+        <li> <input type="text" name="Titulli" value="{{ old('Titulli')}}" id=""></li>
+        <li>Autori</li>
+        <li> <input type="text" name="Autori" value="{{ old('Autori')}}" id=""></li>
+        <li>Shtepia Botuese</li>
+        <li> <input type="text" name="ID_ShtepiaBotuese" value="{{ old('ID_ShtepiaBotuese')}}" id=""></li>
+    </div>
+
+<div class="col">
+       
+            
+            @foreach($librat as $libri)
+            @foreach($libri->ShtepiaBotuese as $shtepia)
+            @if ($shtepia != null)
+            <li>
+            <option value="">Shtepia Botuese</option>
+            <option value="{{$shtepia->ShtepiaBotuese_Emri}}">{{$shtepia->ShtepiaBotuese_Emri}}</option>
+            </li>
+            @endif
+            @endforeach
+            @endforeach
         
-        <td>
-        @foreach($librat as $libri)
-        <option value="">Shtepia Botuese</option>
-        @foreach($libri->ShtepiaBotuese as $shtepia)
-        <option value="{{$shtepia->ShtepiaBotuese_Emri}}">{{$shtepia->ShtepiaBotuese_Emri}}</option>
-        @endforeach
-        @endforeach
-        </td>
 
 
 
 
-        <th>Numri i Faqeve</th>
-        <td> <input type="text" name="NumriIFaqeve" value="{{ old('NumriIFaqeve')}}" id=""></td>
-        <th>Viti i Botimit</th>
-        <td>  <input type="date" name="VitiIBotimit" value="{{ old('VitiIBotimit')}}" id=""></td>
-        <th>Viti i Ribotimit</th>
-        <td><input type="date" name="VitiIRibotimit" value="{{ old('VitiIRibotimit')}}" id=""></td>
-        <th>Vellimi</th>
-        <td><input type="text" name="Vellimi" value="{{ old('Vellimi')}}" id=""></td>
-        <th>Rafti</th>
-        <td><input type="text" name="ID_Rafti" value="{{ old('ID_Rafti')}}" id=""></td>
-        <th>Statusi</th>
-        <td><input type="text" name="Statusi" value="{{ old('Statusi')}}" id=""></td>
-        <th>Numri i Lexuesve</th>
-        <td><input type="text" name="NumriILexuesve" value="{{ old('NumriILexuesve')}}" id=""></td>
-        <td><input type="submit" value="submit"> </td>
-        
-        </table>
+        <li>Numri i Faqeve</li>
+        <li> <input type="text" name="NumriIFaqeve" value="{{ old('NumriIFaqeve')}}" id=""></li>
+        <li>Viti i Botimit</li>
+        <li>  <input type="date" name="VitiIBotimit" value="{{ old('VitiIBotimit')}}" id=""></li>
+    </div>
+<div class="col">
+        <li>Viti i Ribotimit</li>
+        <li><input type="date" name="VitiIRibotimit" value="{{ old('VitiIRibotimit')}}" id=""></li>
+        <li>Vellimi</li>
+        <li><input type="text" name="Vellimi" value="{{ old('Vellimi')}}" id=""></li>
+        <li>Rafti</li>
+        <li><input type="text" name="ID_Rafti" value="{{ old('ID_Rafti')}}" id=""></li>
+    </div>
+
+    <div class="col">
+        <li>Statusi</li>
+        <li><input type="text" name="Statusi" value="{{ old('Statusi')}}" id=""></li>
+        <li>Numri i Lexuesve</li>
+        <li><input type="text" name="NumriILexuesve" value="{{ old('NumriILexuesve')}}" id=""></li>
+        <li><input type="submit" value="submit"> </li>
+    </div>
+    </nav>
+</div>
+</ul>
         @if ($errors->any())
 <div class="notification is-danger">
 
@@ -69,8 +95,9 @@ use LSM\Libri;
 
 </div>
 @endif
+</div>
     </form>
-
+</div>
 @if ($librat->Count())
 Librat:
 <ul>
